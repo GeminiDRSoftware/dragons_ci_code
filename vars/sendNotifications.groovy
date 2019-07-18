9f3abddbffe1c446af42dce66f481f07d8c72b21
Bruno Quint <bquint@gemini.edu>
2019-07-18 09:42:32 -0400

    There were too many files

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -1,30 +1,36 @@
-#!/usr/bin/env groovy
-
-/**
- * Send notifications based on build status string
+#!/usr/bin/groovy
+/*
+ * Send a message to Slack saying that the jobs has started
  */
-def call(String buildStatus = 'STARTED') {
-  // build status of null means successful
-  buildStatus =  buildStatus ?: 'SUCCESSFUL'
 
-  // Default values
-  def color = 'RED'
-  def colorCode = '#FF0000'
-  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def summary = "${subject} (${env.BUILD_URL})"
-  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
 
-  // Override default values based on build status
-  if (buildStatus == 'STARTED') {
-    color = 'YELLOW'
-    colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESSFUL') {
-    color = 'GREEN'
-    colorCode = '#00FF00'
-  }
+def sendNotifications(String buildStatus = 'STARTED') {
 
-  // Send notifications
-  slackSend (color: colorCode, message: summary)
+    // build status of null means successful
+    buildStatus =  buildStatus ?: 'SUCCESSFUL'
 
-}
+    // Default values
+    def color = 'RED'
+    def colorCode = '#FF0000'
+    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+    def summary = "${subject} (${env.BUILD_URL})"
+    def details = """
+        <p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+        <p>Check console output at &QUOT;
+        <a href='${env.BUILD_URL}'>${env.JOB_NAME}
+        [${env.BUILD_NUMBER}]</a>&QUOT;</p>
+        """
+
+    // Override default values based on build status
+    if (buildStatus == 'STARTED') {
+        color = 'YELLOW'
+        colorCode = '#FFFF00'
+    } else if (buildStatus == 'SUCCESSFUL') {
+        color = 'GREEN'
+        colorCode = '#00FF00'
+    }
+
+    // Send notifications
+    slackSend (color: colorCode, message: summary)
+
+ }
