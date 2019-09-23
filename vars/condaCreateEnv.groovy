d634d1706e1cb02cc0ce3462583e3ea46888cdd2
bquint <bquint@gemini.edu>
2019-09-23 17:15:23 -0300

    Adding dummy strings to be echoed by custom step

--- vars/condaCreateEnv.groovy
+++ vars/condaCreateEnv.groovy
@@ -3,9 +3,16 @@
 /**
  * Send notifications based on build status string
  */
+
+/*
 def call(String env_name, String env_file) {
 
     echo "Create Conda Environment: ${env_name}"
     echo "Using file: ${env_file}"
 
+}
+*/
+
+def call() {
+    echo "I am running from inside a custom step! That's so cool! \\o/"
 }
