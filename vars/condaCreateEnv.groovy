ab21361663e5dd89a25f5b34d09b2779e49cff4c
bquint <bquint@gemini.edu>
2019-09-23 17:28:20 -0300

    Trying to parse a string

--- vars/condaCreateEnv.groovy
+++ vars/condaCreateEnv.groovy
@@ -5,6 +5,10 @@
 
 def call(String env_name, String env_file) {
     echo "I am running from inside a custom step! That's so cool! \\o/"
+
+    //noinspection GroovyUnusedAssignment
+    String parsedString = new String("And now this step received an argument: $env_name")
+
     echo "And now this step received an argument: $env_name"
     echo "And this is a second argument: $env_file"
 }
