819beaede5a8549477451f47b31b5e1b46b93b7e
bquint <bquint@gemini.edu>
2019-09-23 17:41:12 -0300

    Convert GString to String first

--- vars/condaCreateEnv.groovy
+++ vars/condaCreateEnv.groovy
@@ -7,8 +7,8 @@ def call(String env_name, String env_file) {
     echo "I am running from inside a custom step! That's so cool! \\o/"
 
     //noinspection GroovyUnusedAssignment
-    String parsedString = new String("And now this step received an argument: $env_name")
+    String parsedString = "And now this step received an argument: $env_name".toString()
 
-    echo parsedString.toString()
+    echo parsedString
     echo "And this is a second argument: ${env_file}"
 }
