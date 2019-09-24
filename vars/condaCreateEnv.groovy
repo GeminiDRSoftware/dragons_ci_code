43c8339ad468956e053752352cf27145baca6b87
bquint <bquint@gemini.edu>
2019-09-24 10:49:43 -0300

    Commented out other steps to isolate reading resources

--- vars/condaCreateEnv.groovy
+++ vars/condaCreateEnv.groovy
@@ -6,15 +6,15 @@
 def call(String env_name, String env_file) {
 
     def env_definition = libraryResource "gemini/dragons/envs/${env_file}"
-    File conda_file = new File("${env_file}")
-    conda_file.write env_definition
+//    File conda_file = new File("${env_file}")
+//    conda_file.write env_definition
 
-    sh  """
-        if conda info --envs | grep -q ${env_name}; then
-            echo " Skipping creation of existing conda environment: ${env_name}";
-        else 
-            conda env create --quiet --file ${env_file} -n ${env_name};
-        fi
-        """
+//    sh  """
+//        if conda info --envs | grep -q ${env_name}; then
+//            echo " Skipping creation of existing conda environment: ${env_name}";
+//        else
+//            conda env create --quiet --file ${env_file} -n ${env_name};
+//        fi
+//        """
 
 }
