dc482d6a9e9b3343d200b7048d315ef6c8790540
bquint <bquint@gemini.edu>
2019-10-02 11:15:54 -0300

    Object construction again with env

--- src/gemini/dragons/GmosArcTests.groovy
+++ src/gemini/dragons/GmosArcTests.groovy
@@ -7,10 +7,12 @@ package gemini.dragons
 
 class GmosArcTests implements Serializable {
 
+    def env
     def steps
     private String _path_to_plots
 
-    GmosArcTests(steps) {
+    GmosArcTests(env, steps) {
+        this.env = env
         this.steps = steps
         this._path_to_plots = "process_arcs/GMOS/tst_gmos_lsspec_arcs.tar.gz"
     }
