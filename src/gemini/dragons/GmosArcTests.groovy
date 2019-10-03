ee8c1e8fdcf72b859fa0d96ab59742eb1c2ff905
bquint <bquint@gemini.edu>
2019-10-03 12:10:37 -0300

    Updated GmosArcTests

--- src/gemini/dragons/GmosArcTests.groovy
+++ src/gemini/dragons/GmosArcTests.groovy
@@ -7,37 +7,14 @@ package gemini.dragons
 
 class GmosArcTests implements Serializable {
 
-    def env
     def steps
-    private String _path_to_plots
 
-    GmosArcTests(env, steps) {
-        this.env = env
-        this.steps = steps
-        this._path_to_plots = "process_arcs/GMOS/test_gmos_lsspec_arcs.tar.gz"
-    }
+    GmosArcTests(steps) { this.steps = steps }
 
     void archivePlots() {
 
         steps.echo "Running 'archivePlots' from inside GmosArcTests"
-
-        def sourceFile = new File(env.DRAGONS_TEST_OUTPUTS, this._path_to_plots)
-        def targetFolder = new File("plots/")
-        def targetFile = new File(targetFolder, sourceFile.getName())
-
-        if (! targetFolder.exists()) {
-            targetFolder.mkdir()
-        }
-
-        if (sourceFile.exists()) {
-            steps.echo "Confirmed that file exists: ${sourceFile}"
-            steps.echo "Will copy it to the target file: ${targetFile}"
-            steps.sh "cp ${targetFile} ${sourceFile}"
-
-//             steps.archiveArtifacts tarFile.toString()
-        } else {
-            steps.echo "Could not find file: ${sourceFile}"
-        }
+        steps.archiveArtifacts artifacts: "plots/*", allowEmptyArchive: true
 
     }
 
