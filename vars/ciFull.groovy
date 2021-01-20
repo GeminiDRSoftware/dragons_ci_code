54e8a5735cb925bc9453baeac724f062c3f677d9
Bruno Quint <b1quint@gmail.com>
2021-01-20 15:46:01 -0300

    Created ciFull.groovy

---
+++ vars/ciFull.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+/**
+ * Checks if the commit message contains the [ci full] string.
+ */
+def call() {
+    result = sh (script: "git log -1 | grep '.*\\[ci full\\].*'", returnStatus: true)
+    return result
+}
